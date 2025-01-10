0cf4e5257a1ec055293e8459d5eed02df5133b59
FarrasW <olibolimoli99@gmail.com>
2025-01-10 03:15:49 +0300

    Create server.py
    Flask basic

---
+++ server.py
@@ -0,0 +1,12 @@
+# Import the Flask class from the flask module
+from flask import Flask, jsonify
+
+# Create an instance of the Flask class, passing in the name of the current module
+app = Flask(__name__)
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def index():
+    # Function that handles requests to the root URL
+    # Creta a dictionary to return as a response
+    return {"message": "Hello World!"}
